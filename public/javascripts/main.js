830c79aab481a297e4417795901a598b96f62ace
Justin Huang <justinhuang777@gmail.com>
2020-05-17 22:46:08 +0800

    fix search scrollIntoView function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,3 @@
-function onFocus() {
-  document.querySelector('#blur').classList.add('blury')
-  console.log(123)
-}
-
-function onFocusOut() {
-  document.querySelector('#blur').classList.remove('blury')
-  console.log(321)
-}
-
 //刪除前再次確認
 function deleteCheckAgain() {
   return window.confirm('Do you really want to delete this restaurant ?')
@@ -19,7 +9,7 @@ function editCheckAgain() {
 
 const pathname = window.location.pathname
 if (pathname === '/search') {
-  const searchBar = document.querySelector('.search-bar-outer')
+  const searchBar = document.querySelector('.search-bar')
   window.addEventListener('load', () => {
     //圖片載入完成才執行滑動
     searchBar.scrollIntoView({ behavior: 'smooth' })
@@ -42,19 +32,29 @@ if (pathname.includes('/restaurants')) {
   })
 }
 
-const dropdownMenu = document.querySelector('.dropdown-menu')
-const dropdownBtn = document.querySelectorAll('.dropdown-btn')
+// const dropdownMenu = document.querySelector('.dropdown-menu')
+// const dropdownBtn = document.querySelectorAll('.dropdown-btn')
 
-dropdownMenu.addEventListener('click', e => {
-  if (e.target.classList.contains('dropdown-item')) {
-    window.addEventListener('load', () => {
-      window.alert(123)
-      dropdownBtn.innerText = e.target.innerText
-      // e.target.classList.add('active')
-    })
-  }
-})
+// dropdownMenu.addEventListener('click', e => {
+//   if (e.target.classList.contains('dropdown-item')) {
+//     window.addEventListener('load', () => {
+//       window.alert(123)
+//       dropdownBtn.innerText = e.target.innerText
+//       // e.target.classList.add('active')
+//     })
+//   }
+// })
 
-if (pathname.includes('/sort/name/asc')) {
-  dropdownBtn.innerText = `餐廳名稱 (A ~ Z)`
-}
+// if (pathname.includes('/sort/name/asc')) {
+//   dropdownBtn.innerText = `餐廳名稱 (A ~ Z)`
+// }
+
+// function onFocus() {
+//   document.querySelector('#blur').classList.add('blury')
+//   console.log(123)
+// }
+
+// function onFocusOut() {
+//   document.querySelector('#blur').classList.remove('blury')
+//   console.log(321)
+// }
